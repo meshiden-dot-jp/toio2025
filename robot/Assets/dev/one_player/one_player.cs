028d1e5b43162b663b5847fba8ba85c9d4be6a86
Kanata Takayama <a5823061@aoyama.jp>
2025-07-02 12:35:32 +0900

    多分完成

--- robot/Assets/dev/one_player/one_player.cs
+++ robot/Assets/dev/one_player/one_player.cs
@@ -12,14 +12,14 @@ public class test_1_cs : MonoBehaviour
     public ConnectType connectType;
     CubeManager cubeManager;
     Cube cube;
-    bool s = true, g = false;
-    float intervalTime = 0.2f;
-    float elapsedTime = 0;
+    bool s = true, g = false, gs= true; // s: start, g: goal, gs: goal sound
+    //float intervalTime = 0.2f;
+    //float elapsedTime = 0;
     int collision_count = 0;
     Vector2 startPos = new Vector2(120, 336); // Start position for the cube
-    Vector2 goalPos = new Vector2(98, 142); // Target position for the cube
+    Vector2 goalPos = new Vector2(356, 180); // Target position for the cube
     Vector2 cubePos; // Another target position for the cube
-    bool wasColliding = false, isColliding;
+    bool isColliding;
 
     /*List<Vector3> walls = new List<Vector3>()
     {
@@ -35,7 +35,7 @@ public class test_1_cs : MonoBehaviour
             Vector3 wall = WallData.walls[i];
             if (wall.z == 1)//hoizontal
             {
-                if (nowPos.x >= wall.x && nowPos.x <= wall.x + 43 && nowPos.y >= wall.y && nowPos.y <= wall.y + 10)
+                if (nowPos.x >= wall.x && nowPos.x <= wall.x + 43 && nowPos.y >= wall.y-5 && nowPos.y <= wall.y + 5)
                 {
                     Debug.Log("Collision with wall at: " + wall);
                     return true;
@@ -43,7 +43,7 @@ public class test_1_cs : MonoBehaviour
             }
             else if (wall.z == 0)//vertical
             {
-                if (nowPos.x >= wall.x && nowPos.x <= wall.x + 10 && nowPos.y <= wall.y && nowPos.y >= wall.y - 43)
+                if (nowPos.x >= wall.x-5 && nowPos.x <= wall.x + 5 && nowPos.y <= wall.y && nowPos.y >= wall.y - 43)
                 {
                     Debug.Log("Collision with wall at: " + wall);
                     return true;
@@ -53,12 +53,28 @@ public class test_1_cs : MonoBehaviour
         return false;
     }
 
-    /*void fanfare(Cube c)
+
+    //Goal sound for good score(=low collision count)
+    void fanfare(Cube c)
 	{
-		
-		c.PlaySound(1, sound.ToArray);
-	
-	}*/
+		 List<Cube.SoundOperation> sound = new List<Cube.SoundOperation>();
+        sound.Add(new Cube.SoundOperation(durationMs:100, volume:15, note_number:65));
+        sound.Add(new Cube.SoundOperation(durationMs:30, volume:15, note_number:Cube.NOTE_NUMBER.NO_SOUND));  
+        sound.Add(new Cube.SoundOperation(durationMs:100, volume:15, note_number:65));
+        sound.Add(new Cube.SoundOperation(durationMs:30, volume:15, note_number:Cube.NOTE_NUMBER.NO_SOUND));            
+        sound.Add(new Cube.SoundOperation(durationMs:100, volume:15, note_number:65));
+        sound.Add(new Cube.SoundOperation(durationMs:30, volume:15, note_number:Cube.NOTE_NUMBER.NO_SOUND));      
+        sound.Add(new Cube.SoundOperation(durationMs:100, volume:15, note_number:65));  
+        sound.Add(new Cube.SoundOperation(durationMs:170, volume:15, note_number:Cube.NOTE_NUMBER.NO_SOUND));
+        sound.Add(new Cube.SoundOperation(durationMs:100, volume:15, note_number:63));
+        sound.Add(new Cube.SoundOperation(durationMs:130, volume:15, note_number:Cube.NOTE_NUMBER.NO_SOUND));  
+        sound.Add(new Cube.SoundOperation(durationMs:100, volume:15, note_number:67));
+        sound.Add(new Cube.SoundOperation(durationMs:130, volume:15, note_number:Cube.NOTE_NUMBER.NO_SOUND));            
+        sound.Add(new Cube.SoundOperation(durationMs:300, volume:15, note_number:65));
+  
+            // 楽譜を再生
+        c.PlaySound(1, sound.ToArray());
+	}
 
 
     async void Start()
@@ -77,7 +93,6 @@ public class test_1_cs : MonoBehaviour
     void Update()
     {
         Console.WriteLine("Update called");
-        //elapsedTime += Time.deltaTime;
 
         if (ws == null)
         {
@@ -115,28 +130,34 @@ public class test_1_cs : MonoBehaviour
                     }
                 }
             }
+            //goal
             else if (g)
             {
-                Debug.Log("GOAL");
-                cube.Move(50, -50, 100);
-                /*if(collision_count<=100)
-					//fanfare(cube);
-					cube.PlayPresetSound(8);
-				else*/
-                cube.PlayPresetSound(9);
+                //goal sound (once)
+                if (gs)
+                {
+                    Debug.Log("GOAL");
+                    fanfare(cube);
+                    gs = false;
+                }
             }
+            //playing
             else
             {
                 cubePos = new Vector2(cube.x, cube.y);
                 isColliding = Collision(cubePos);
+                //collision detection
                 if (isColliding)
                 {
-                    cube.PlayPresetSound(0);
-                    //cube.TurnLedOn(255, 0, 0);
                     collision_count++;
-            
-                } 
-                else if (cubePos == goalPos)
+                    //collision sound
+                    if (collision_count % 20 == 0)
+                    {
+                        cube.PlayPresetSound(0);
+                    }
+                }
+                //goal detection
+                else if (cubePos.x >= goalPos.x && cubePos.y <= goalPos.y)
                 {
                     g = true;
                 }
